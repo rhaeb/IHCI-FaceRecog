39977f26563d814a95ddef47a5138b908c0fbc5b
rhaeb <rhaebanna@gmail.com>
2024-12-03 18:46:20 +0800

    edit

---
+++ src/db copy.js
@@ -0,0 +1,18 @@
+const { Client } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const client = new Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: "123456",
+});
+
+client.connect()
+  .then(() => console.log("Connected to PostgreSQL"))
+  .catch(err => console.error("Connection error", err.stack));
+
+module.exports = client;
