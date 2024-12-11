63456dec5f907f33559ca8aa033304d5623afe2b
rhaeb <rhaebanna@gmail.com>
2024-12-11 22:13:32 +0800

    fetch

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -11,8 +11,12 @@ router.post('/login', userController.loginUser);
 router.get('/:userId', authenticateToken, userController.findUser);
 router.get('/details/:userId', authenticateToken, userController.getUserDetails);
 router.put('/:id', authenticateToken, userController.updateUser);
+//router.post('/logout', authenticateToken, logoutUser);
 
 // New Route for Email/Password Login
 router.post('/login-password', userController.loginWithPassword);
+
+router.post('/verify-token', authenticateToken, userController.verifyToken);
+
 
 module.exports = router;
